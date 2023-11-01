c9e6814eb4c9e135e82406f76a7e8757e6710305
Andy <atdiegaardt@gmail.com>
2023-11-01 13:27:06 +0200

    Add function to update API and display city weather

---
+++ app.js
@@ -0,0 +1,22 @@
+const apiKey = "2d9d1988f5af14e1ae7592a2120e2485";
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+  const response = await fetch(baseURL + city + `&appid=${apiKey}`);
+  var data = await response.json();
+
+  console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+})
+
